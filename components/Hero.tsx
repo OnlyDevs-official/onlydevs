f913b73193e2ee44125eac8257d46239265be809
Your Name <you@example.com>
2025-08-24 23:28:04 +0530

    Layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,52 +41,6 @@ const HeroSection = () => {
                   exploration, and problem-solving within the design &
                   development communities.
                 </motion.p>
-                <div className="flex space-x-4">
-                  <Link
-                    href={"https://www.linkedin.com/company/onlydevsofficial"}
-                    target="_blank"
-                    aria-placeholder="LinkedIn social"
-                  >
-                    <Button>
-                      <LinkedInLogoIcon className="w-5 h-5 sm:w-7 sm:h-7 md:w-10 md:h-10" />
-                    </Button>
-                  </Link>
-                  <Link
-                    href={"https://www.instagram.com/onlydevsofficial"}
-                    target="_blank"
-                    aria-placeholder="Instagram social"
-                  >
-                    <Button>
-                      <InstagramLogoIcon className="w-5 h-5 sm:w-7 sm:h-7 md:w-10 md:h-10" />
-                    </Button>
-                  </Link>
-                  <Link
-                    href={"https://x.com/onlydevsoffic_"}
-                    target="_blank"
-                    aria-placeholder="X social"
-                  >
-                    <Button>
-                      <svg
-                        width="30"
-                        height="30"
-                        viewBox="0 0 116 104"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5 sm:w-7 sm:h-7 md:w-full md:h-full "
-                      >
-                        <path
-                          d="M91.3013 0H108.944L70.4 44.0533L115.744 104H80.24L52.432 67.6427L20.6133 104H2.95999L44.1867 56.88L0.687988 0H37.0933L62.2293 33.232L91.3013 0ZM85.1093 93.44H94.8853L31.7813 10.0053H21.2907L85.1093 93.44Z"
-                          fill="white"
-                        />
-                      </svg>
-                    </Button>
-                  </Link>
-                  <Link href={"/about-us"} aria-placeholder="About Us">
-                    <Button>
-                      <PersonIcon className="w-5 h-5 sm:w-7 sm:h-7 md:w-10 md:h-10" />
-                    </Button>
-                  </Link>
-                </div>
                 <AnimatedButton />
               </div>
               <motion.div
@@ -138,16 +92,3 @@ const AnimatedButton = () => {
     </div>
   );
 };
-
-function Button({ children, ...prop }: { children: ReactNode }) {
-  return (
-    <>
-      <button
-        className="h-14 w-14 sm:h-16 sm:w-16 md:h-20 md:w-20 rounded-full border border-white flex justify-center items-center text-2xl md:text-4xl font-bold"
-        {...prop}
-      >
-        <span className="hover:scale-[1.1] transition">{children}</span>
-      </button>
-    </>
-  );
-}

097e7491995fa0dfd6be7705ec86232ba38ade85
Your Name <you@example.com>
2025-08-24 13:33:05 +0530

    Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OnlyDevs",

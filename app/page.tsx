2275aeff22d9ee15462f7c0fe150ad89ed99718a
Your Name <you@example.com>
2025-08-25 23:45:02 +0530

    Events

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <div className="bg-black min-h-screen">
         <HeroSection />
-
+        <EventsCarousel />
         <FeaturesList />
         <FAQSection />
       </div>

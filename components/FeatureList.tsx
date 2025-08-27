8f0f2392cfa6162c323e358c563b26b707ddf5d5
Mayank Rawat <you@example.com>
2025-08-28 00:11:46 +0530

    Features

--- components/FeatureList.tsx
+++ components/FeatureList.tsx
@@ -5,7 +5,8 @@ import Image, { StaticImageData } from "next/image";
 import { features } from "@/lib/consts";
 
 const FeaturesList = () => {
-  const titleRef = useRef(null);
+  // Title animations
+  const titleRef = useRef<HTMLHeadingElement | null>(null);
   const { scrollYProgress: titleScrollProgress } = useScroll({
     target: titleRef,
     offset: ["start end", "end start"],
@@ -27,10 +28,37 @@ const FeaturesList = () => {
     ["blur(4px)", "blur(0px)", "blur(0px)", "blur(4px)"]
   );
 
+  // Paragraph animations
+  const paraRef = useRef<HTMLParagraphElement | null>(null);
+  const { scrollYProgress: paraScrollProgress } = useScroll({
+    target: paraRef,
+    offset: ["start end", "end start"],
+  });
+
+  const paraOpacity = useTransform(
+    paraScrollProgress,
+    [0, 0.2, 0.8, 1],
+    [0, 1, 1, 0]
+  );
+  const paraScale = useTransform(
+    paraScrollProgress,
+    [0, 0.2, 0.8, 1],
+    [0.8, 1, 1, 0.8]
+  );
+  const paraFilter = useTransform(
+    paraScrollProgress,
+    [0, 0.2, 0.8, 1],
+    ["blur(4px)", "blur(0px)", "blur(0px)", "blur(4px)"]
+  );
+
   return (
     <>
-      <section id="featureList" className=" mt-[12rem] py-24 px-4 sm:px-6 lg:px-8">
-        <motion.h1 ref={titleRef}
+      <section
+        id="featureList"
+        className="mt-[12rem] py-24 px-4 sm:px-6 lg:px-8"
+      >
+        <motion.h1
+          ref={titleRef}
           className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center max-w-6xl mx-auto mb-20 aeonik-bold"
           style={{
             opacity: titleOpacity,
@@ -42,15 +70,19 @@ const FeaturesList = () => {
         </motion.h1>
 
         <motion.p
+          ref={paraRef}
           className="text-lg md:text-xl text-center text-white font-light max-w-3xl mx-auto mb-12"
           style={{
-            opacity: titleOpacity,
-            scale: titleScale,
-            filter: titleFilter,
+            opacity: paraOpacity,
+            scale: paraScale,
+            filter: paraFilter,
           }}
         >
-          Network with <span className="orbikular-medium">real industry experts</span> who know what they're talking about.
+          Network with{" "}
+          <span className="orbikular-medium">real industry experts</span> who
+          know what they're talking about.
         </motion.p>
+
         <div className="mx-auto max-w-7xl">
           {features.map((feature, index) => (
             <FeatureItem key={feature.title} feature={feature} index={index} />
@@ -61,7 +93,7 @@ const FeaturesList = () => {
   );
 };
 
-interface featureItemProps {
+interface FeatureItemProps {
   feature: {
     title: string;
     description: string;
@@ -70,8 +102,8 @@ interface featureItemProps {
   index: number;
 }
 
-const FeatureItem = ({ feature, index }: featureItemProps) => {
-  const ref = useRef(null);
+const FeatureItem = ({ feature, index }: FeatureItemProps) => {
+  const ref = useRef<HTMLDivElement | null>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start end", "end start"],
@@ -95,7 +127,7 @@ const FeatureItem = ({ feature, index }: featureItemProps) => {
         <div
           className={`${
             index % 2 === 1 ? "md:order-2" : ""
-          } bg-gradient-to-t from-gradientColor  to-[#0F1015] rounded-xl`}
+          } bg-gradient-to-t from-gradientColor to-[#0F1015] rounded-xl`}
         >
           <Image
             src={feature.image}

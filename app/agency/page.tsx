f7d4b5c05639b10fbb1ae51be1da2c6d234ad5c7
OnlyDevs-official <onlydevsofficial@gmail.com>
2025-10-06 10:05:30 +0530

    Update page.tsx

--- app/agency/page.tsx
+++ app/agency/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { ReactNode, useState } from "react";
 import ServicesCarousel from "@/components/services";
+import styles from './agency.module.css';
 
 function agency(){
     return(
         <>
-            <h1>Our Agency</h1>
+            <h1 id="agencyheading">Our Agency</h1>
             <ServicesCarousel/>
         </>
     )

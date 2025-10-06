9e7feb1a066fc9532dd0923a6141199436210335
OnlyDevs-official <onlydevsofficial@gmail.com>
2025-10-06 10:13:47 +0530

    Update page.tsx

--- app/agency/page.tsx
+++ app/agency/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { ReactNode, useState } from "react";
 import ServicesCarousel from "@/components/services";
-import styles from './agency.module.css';
+import './agency_globals.css';
 
 function agency(){
     return(

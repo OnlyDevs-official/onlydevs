dcee00018610d30e1659bc7607cbd01c4ff3589f
OnlyDevs-official <onlydevsofficial@gmail.com>
2025-10-07 16:32:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <>
       <div className= "min-h-screen">
         <div id="secondnav">
-        <button onClick={() => window.location.href = '/#blogsection'} id="blogbutton">Blog</button>
+        <button id="blogbutton">Blog</button>
         </div>
       
         <HeroSection />
